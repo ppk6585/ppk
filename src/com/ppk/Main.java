186384ead935d6c54b1bf6657e959fb7ce7e45e6
prkulk <ppk6585@gmail.com>
2015-04-08 20:10:50 -0500

    new method introduced to add 2 numbers

--- src/com/ppk/Main.java
+++ src/com/ppk/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello world");
+        addTwoNumbers();
 
     }
+
+    public static void addTwoNumbers(){System.out.print(2+3);}
 }
